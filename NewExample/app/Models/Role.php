c2d3c7ad27010fbee574e89ed57c613d2c1bd9a8
Javier Neira <javneira@gmail.com>
2017-09-10 23:21:35 -0300

    [models] modificacion Roles

--- NewExample/app/Models/Role.php
+++ NewExample/app/Models/Role.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
+    use SoftDeletes;
+    protected $guarded=['id'];
+    protected $fillable=['name','code'];
+
+    public function users(){
+        return $this->hasMany('users');
+    }
 
 }
